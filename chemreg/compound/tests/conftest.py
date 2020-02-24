eedd0d7066d25dde0485dc3303460a60a613fb5c
Matt Covalt <mcovalt@mailbox.org>
2020-02-24 17:28:12 -0500

    Add tests

--- chemreg/compound/tests/conftest.py
+++ chemreg/compound/tests/conftest.py
@@ -1,23 +1,27 @@
-import factory
 import pytest
 
-from chemreg.compound.models import BaseCompound
+from chemreg.compound.models import DefinedCompound
+from chemreg.compound.serializers import DefinedCompoundSerializer
 from chemreg.compound.settings import compound_settings
-from chemreg.compound.tests.factories import BaseCompoundFactory
+from chemreg.compound.tests.factories import (
+    DefinedCompoundFactory,
+    DefinedCompoundJSONFactory,
+)
+
+COMPOUND_REGISTRY = {
+    "DefinedCompound": {
+        "factory": DefinedCompoundFactory,
+        "json_factory": DefinedCompoundJSONFactory,
+        "model": DefinedCompound,
+        "serializer": DefinedCompoundSerializer,
+    }
+}
 
 
-@pytest.fixture
-def basecompound() -> BaseCompound:
-    """A manufactured BaseCompound model."""
-    return BaseCompoundFactory.build()
-
-
-@pytest.fixture
-def basecompound_json() -> dict:
-    """A manufactured BaseCompound JSON."""
-    d = factory.build(dict, FACTORY_CLASS=BaseCompoundFactory)
-    d["id"] = d.pop("cid")
-    return d
+@pytest.fixture(params=COMPOUND_REGISTRY.keys())
+def compound(request):
+    """The registry entry for all subclasses of `BaseCompound`."""
+    return COMPOUND_REGISTRY[request.param]
 
 
 INVALID_CIDS = [
@@ -35,4 +39,26 @@ INVALID_CIDS = [
 @pytest.fixture(params=INVALID_CIDS)
 def invalid_cid(request) -> str:
     """An invalid CID."""
+    return request.param
+
+
+INVALID_INCHIKEYS = [
+    f"{'A'*13}-{'A'*10}-{'A'*1}",  # wrong character count - 0
+    f"{'A'*14}-{'A'*7}-{'A'*1}",  # wrong character count - 1
+    f"{'A'*14}-{'A'*10}-{'A'*2}",  # wrong character count - 2
+    f"{'A'*14}-{'A'*10}{'A'*1}",  # missing dash - 0
+    f"{'A'*14}{'A'*10}-{'A'*1}",  # missing dash - 1
+    f"{'1'*14}{'A'*10}-{'A'*1}",  # numbers not letters - 0
+    f"{'A'*14}{'1'*10}-{'A'*1}",  # numbers not letters - 1
+    f"{'A'*14}{'A'*10}-{'1'*1}",  # numbers not letters - 2
+    f"{'a'*14}{'A'*10}-{'A'*1}",  # lowercase - 0
+    f"{'A'*14}{'a'*10}-{'A'*1}",  # lowercase - 1
+    f"{'A'*14}{'A'*10}-{'a'*1}",  # lowercase - 2
+]
+"""A collection of invalid InchiKeys."""
+
+
+@pytest.fixture(params=INVALID_INCHIKEYS)
+def invalid_inchikey(request) -> str:
+    """An invalid InchiKey string."""
     return request.param
