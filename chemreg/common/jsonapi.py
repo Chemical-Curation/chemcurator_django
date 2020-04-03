00927c4a38d5bee4c2b804e5236a086c1cadcb0d
Matt Covalt <mcovalt@mailbox.org>
2020-04-03 17:21:28 -0400

    Add "self" link

--- chemreg/common/jsonapi.py
+++ chemreg/common/jsonapi.py
@@ -9,11 +9,22 @@ class RootMetaMixin:
         return {"apiVersion": "1.0.0-beta"}
 
 
-class HyperlinkedModelSerializer(serializers.HyperlinkedModelSerializer, RootMetaMixin):
+class SelfLinkMixin:
+    """Adds a link to "self" for a serializer."""
+
+    def __new__(cls, *args, **kwargs):
+        if "url" not in cls.Meta.fields:
+            cls.Meta.fields += type(cls.Meta.fields)(("url",))
+        return super().__new__(cls, *args, **kwargs)
+
+
+class HyperlinkedModelSerializer(
+    serializers.HyperlinkedModelSerializer, SelfLinkMixin, RootMetaMixin
+):
     pass
 
 
-class ModelSerializer(serializers.ModelSerializer, RootMetaMixin):
+class ModelSerializer(serializers.ModelSerializer, SelfLinkMixin, RootMetaMixin):
     pass
 
 
@@ -23,6 +34,7 @@ class PolymorphicModelSerializer(
         serializers.PolymorphicModelSerializer.__bases__,
         dict(vars(serializers.PolymorphicModelSerializer)),
     ),
+    SelfLinkMixin,
     RootMetaMixin,
     metaclass=serializers.PolymorphicSerializerMetaclass,
 ):
