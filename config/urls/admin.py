30aa0c21e9ad9a96fb4d24ea131737d131e3bfe7
AScaduto <Anthony.Scaduto@gdit.com>
2020-07-01 11:19:30 -0400

    initial config for metrics endpoint

--- config/urls/admin.py
+++ config/urls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, url
 
 urlpatterns = [
     path("", admin.site.urls),
+    url("", include("django_prometheus.urls")),
 ]
--- config/urls/api.py
+++ config/urls/api.py
@@ -1,7 +1,8 @@
-from django.urls import include, path
+from django.urls import include, path, url
 
 urlpatterns = [
     path("", include("chemreg.openapi.urls")),
     path("", include("chemreg.auth.urls")),
     path("", include("chemreg.compound.urls")),
+    url("", include("django_prometheus.urls")),
 ]
