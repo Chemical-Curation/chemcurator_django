fc970e4d8ffbd556406e3289bf168df34317df7f
AScaduto <Anthony.Scaduto@gdit.com>
2020-07-15 11:27:15 -0400

    source serializer and view tests

--- chemreg/substance/tests/test_serializers.py
+++ chemreg/substance/tests/test_serializers.py
@@ -2,6 +2,7 @@ import pytest
 
 from chemreg.substance.serializers import (
     HyperlinkedModelSerializer,
+    SourceSerializer,
     SynonymTypeSerializer,
 )
 
@@ -14,5 +15,17 @@ def test_synonym_type_serializer():
 @pytest.mark.django_db
 def test_synonym_type(synonym_type_factory):
     serializer = synonym_type_factory.build()
+    assert serializer.is_valid()
+    serializer.save()
+
+
+@pytest.mark.django_db
+def test_source_serializer():
+    assert issubclass(SourceSerializer, HyperlinkedModelSerializer)
+
+
+@pytest.mark.django_db
+def test_source(source_factory):
+    serializer = source_factory.build()
     assert serializer.is_valid()
     serializer.save()
--- chemreg/substance/tests/test_views.py
+++ chemreg/substance/tests/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 
-from chemreg.substance.views import ModelViewSet, SynonymTypeViewSet
+from chemreg.substance.views import ModelViewSet, SourceViewSet, SynonymTypeViewSet
 
 
 def test_synonym_type_view():
@@ -31,3 +31,31 @@ def test_synonym_type_get(client, admin_user, synonym_type_factory):
         assert "short_description" in result
         assert "long_description" in result
         assert "score_modifier" in result
+
+
+def test_source_view():
+    """Tests that the Source View Set, is a sub class of the Model View Set."""
+    assert issubclass(SourceViewSet, ModelViewSet)
+
+
+@pytest.mark.django_db
+def test_source_post(client, admin_user, source_factory):
+    client.force_authenticate(user=admin_user)
+    stf = source_factory.build()
+    resp = client.post(
+        "/sources", {"data": {"type": "source", "attributes": stf.initial_data}},
+    )
+    assert resp.status_code == 201
+
+
+@pytest.mark.django_db
+def test_source_get(client, admin_user, source_factory):
+    client.force_authenticate(user=admin_user)
+    source_factory.create()
+    resp = client.get("/sources")
+    assert resp.status_code == 200
+    for result in resp.data["results"]:
+        assert "name" in result
+        assert "label" in result
+        assert "short_description" in result
+        assert "long_description" in result
